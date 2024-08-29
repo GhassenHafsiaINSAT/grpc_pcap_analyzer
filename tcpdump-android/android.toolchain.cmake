2b7fc828b812487dcad25f69d1066c51880d3dac
HAFSIA GHASSEN <110825502+GhassenHafsiaINSAT@users.noreply.github.com>
2024-08-29 14:41:35 +0100

    CMakeLists.txt for tcpdump

--- tcpdump-android/android.toolchain.cmake
+++ tcpdump-android/android.toolchain.cmake
@@ -1,6 +1,4 @@
-cmake_minimum_required(VERSION 3.10)
-
-project(MyAndroidProject C)
+set (CMAKE_SYSTEM_NAME Android)
 
 set(CMAKE_SYSTEM_VERSION 35)
 
@@ -8,14 +6,11 @@ set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
 
 set(CMAKE_ANDROID_NDK /opt/android-ndk-r27)
 
-set(CMAKE_SYSTEM_NAME Android)
-
-set(CMAKE_ANDROID_STL_TYPE c++_shared)
-
 set(CMAKE_C_COMPILER /opt/android-ndk-r27/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang)
 
 set(CMAKE_CXX_COMPILER /opt/android-ndk-r27/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android35-clang++)
 
 set(CMAKE_SYSROOT /opt/android-ndk-r27/toolchains/llvm/prebuilt/linux-x86_64/sysroot)
 
-add_executable(test src/test.c)
+set(CMAKE_ANDROID_STL_TYPE C++_shared)
+
